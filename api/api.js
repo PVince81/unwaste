2177cae9d259c08051730c2f86b842e0edcf7ae7
Vincent Petry <PVince81@yahoo.fr>
2013-07-19 20:41:21 +0200

    Added mysql connection

--- api/api.js
+++ api/api.js
@@ -1,3 +1,45 @@
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'unwaste',
+  password : 'un3W4$13',
+  database : 'unwaste'
+});
+
+/*
+  CREATE DATABASE unwaste;
+  CREATE USER 'unwaste'@'localhost' IDENTIFIED BY 'un3W4$13';
+  GRANT ALL PRIVILEGES ON unwaste.* TO 'unwaste'@'localhost'
+
+  USE unwaste;
+
+  CREATE TABLE wastepoint (
+    id int NOT NULL AUTO_INCREMENT,
+    latitude FLOAT(12, 10) NOT NULL,
+    longitude FLOAT(12, 10) NOT NULL,
+    timestamp datetime NOT NULL,
+    PRIMARY KEY (id)
+  );
+*/
+
 exports.addWastePoint = function(query, callback){
     console.log('addWastePoint', query);
-}
+
+    var values = [
+        query.latitude,
+        query.longitude,
+        query.timestamp
+    ];
+    connection.connect();
+    var sqlQuery = 'INSERT INTO wastepoint (latitude, longitude, timestamp) VALUES (' + values.join(', ') + ')';
+
+    console.log('SQL: ', sqlQuery);
+
+    connection.query(sqlQuery, function(err, rows, fields) {
+        if (err){
+            console.error(err);
+        }
+        connection.end();
+        callback();
+    });
+};
