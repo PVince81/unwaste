80a625095e6118a86478b7574f5de68215bb1edc
Paul Sonnentag <Paul.Sonnentag@googlemail.com>
2013-07-19 20:52:40 +0200

    Added mobile nav with test pages

--- public/src/app.js
+++ public/src/app.js
@@ -14,7 +14,19 @@
       function ($routeProvider) {
 
         $routeProvider
-          .when('/', {templateUrl: '../templates/start.html'})
+          .when('/', {
+            templateUrl: '../templates/start.html',
+            controller: 'startController',
+            transition: 'slide'
+          })
+          .when('/discover', {
+            templateUrl: '../templates/discover.html',
+            transition: 'slide'
+          })
+          .when('/spot', {
+            templateUrl: '../templates/spot.html',
+            transition: 'slide'
+          })
           .otherwise({redirectTo: '/'});
       }
     ])
