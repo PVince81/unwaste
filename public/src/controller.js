2ddcb4baaff51576fd38a77609c2d55885542349
Paul Sonnentag <Paul.Sonnentag@googlemail.com>
2013-07-20 17:10:23 +0200

    Added image upload

--- public/src/controller.js
+++ public/src/controller.js
@@ -20,16 +20,23 @@
 
         function success(data) {
 
-          $http.post('/api/wastepoint', {
-            latitude: data.coords.latitude,
-              longitude: data.coords.longitude,
-              timestamp: Date.now()
-          }).success(function () {
+          $scope.save = function () {
 
-            console.log(data);
+            $http.post('/api/wastepoint', {
+              latitude: data.coords.latitude,
+                longitude: data.coords.longitude,
+                timestamp: Date.now(),
+                img: $scope.imageData,
+                comment:  $scope.comment,
+                todo: 0
+            }).success(function () {
+                alert('successfully created!');
+            });
+          };
 
+          $scope.$on('imageSelected', function (evt, imageData) {
+            $scope.imageData = imageData;
           });
-
         }
 
         function error(err) {
